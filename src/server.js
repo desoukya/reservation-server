988708871eb949e5aa253eb7aba35489d6afd95f
Amr Desouky <desoukya@gmail.com>
2018-02-05 18:30:57 -0500

    allow CORS

--- src/server.js
+++ src/server.js
@@ -11,6 +11,14 @@ const db = require('./data/db');
 const bodyParser  = require('body-parser');
 app.use(bodyParser.json());
 
+// Allow CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET,POST');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // Create HTTP Server and Listen for Requests
 app.listen(PORT, function(req, res) {
   console.log(`[OK] = HTTP Web Server Running On Port ${PORT}`);
