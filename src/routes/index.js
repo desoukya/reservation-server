5d27a36fa94c0088f655e8d9b76963eeaa931b54
Amr Desouky <desoukya@gmail.com>
2018-02-05 20:03:29 -0500

    add /api/v1 to API routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 const reservationsCtrl = require('../controllers/reservation-ctrl');
 
 module.exports = function(app) {
-  app.get('/reservations', reservationsCtrl.getAllReservations);
-  app.get('/reservation/:id', reservationsCtrl.getSingleReservation);
-  app.post('/reservation', reservationsCtrl.createReservation);
+  app.get('/api/v1/reservations', reservationsCtrl.getAllReservations);
+  app.get('/api/v1/reservation/:id', reservationsCtrl.getSingleReservation);
+  app.post('/api/v1/reservation', reservationsCtrl.createReservation);
 };
